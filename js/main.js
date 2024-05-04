81774f396739aa6b30d5dc95083f5beb252299fe
pixelpando <pando100@gmail.com>
2024-05-03 22:39:42 -0300

    v0.45

--- js/main.js
+++ js/main.js
@@ -1,53 +1,3 @@
-//Bolsa de 50 kg.
-let cemento = 50;
-
-//Coeficiente de desperdicio (5%)
-let desperdicio = 1.05;
-
-//Espesor del mortero en mts.
-let junta = 0.015;
-
-const ladrillos = ['Ladrillos Comunes 5x13x25', 'Ladrillos Huecos 6 agujeros 12x18x25', 'Ladrillos Huecos 9 agujeros 18x18x25']
-
-//Medidas de referencia ladrillo común en mts.
-// let altoLadrillo = 0.05;
-// let anchoLadrillo = 0.13;
-// let largoLadrillo = 0.25;
-
-//Funciones
-function calcularLadrillos(altoLadrillo, anchoLadrillo, largoLadrillo, ladrilloNombre) {
-    let longitudPared = parseFloat(prompt('Longitud de todas las paredes en mts. (Ej: 4.2):'));
-    let alturaPared = parseFloat(prompt('Altura de paredes en mts. (Ej: 2.5):'));
-    console.log('********** Desglose de valores **************');
-
-    let resultadoPared = longitudPared * alturaPared;
-    console.log('Pared: '+resultadoPared+ ' m2');
-
-    let ladrilloMortero = ((largoLadrillo + junta) * (altoLadrillo + junta));
-    console.log('Superficie del ladrillo: '+ladrilloMortero+ ' mts.');
-
-    let ladrillosCantidad = resultadoPared / ladrilloMortero * desperdicio;
-    console.log('Cantidad de ladrillos: '+ladrillosCantidad+' unid.');
-
-    // let cementoBolsas = Math.round((ladrillosCantidad / 3) / cemento);
-    let juntaSuperior = junta * largoLadrillo * anchoLadrillo;
-    let juntaLateral = junta * altoLadrillo * anchoLadrillo;
-    let juntaTotal = (juntaSuperior + juntaLateral) * ladrillosCantidad;
-    console.log('Junta total: ' +juntaTotal+ ' m3');
-
-    //Relacion 1:3 - 454 Kg./m3 de cemento
-    let dosificacionMortero = 454;
-    let cementoBolsas = dosificacionMortero * juntaTotal * desperdicio / cemento;
-    console.log('Bolsas de cemento: ' +cementoBolsas+ ' unid.');
-
-    //Redondeo de valores
-    resultadoPared = Math.round(resultadoPared);
-    ladrillosCantidad = Math.round(ladrillosCantidad);
-    cementoBolsas = Math.ceil(cementoBolsas);
-    
-    alert('La superficie de su pared será de apróximadamente '+resultadoPared+' m2. \nNecesitará: \n- '+ladrillosCantidad+' '+ladrilloNombre+'.\n- '+cementoBolsas+' '+txtBolsa+' de cemento de 50 kg.');
-}
-
 // const textoPrompt = 'Que tipo de ladrillos desea llevar: \n(1) '+ladrillos[0]+' \n(2) '+ladrillos[1]+' \n(3) '+ladrillos[2]+' \n(4) Ver lista de Cementos \n(5) Salir';
 // let productoElegido = parseInt(prompt(textoPrompt));
 
@@ -79,54 +29,73 @@ function calcularLadrillos(altoLadrillo, anchoLadrillo, largoLadrillo, ladrilloN
 // **************** NUEVA VERSION ********************
 
 let header = document.getElementsByTagName('header');
-header[0].innerText = 'Calculadora de Materiales v0.43. Última Actualización: 03/05/2024';
+header[0].innerText = 'Calculadora de Materiales v0.45. Última Actualización: 03/05/2024';
 
 let msgDesperdicio = document.getElementById('msg');
 msgDesperdicio.innerHTML = '<i class="fa-solid fa-triangle-exclamation fa-lg"></i>Los datos son a modo de referencia y pueden no ser exactos. Los cálculos <strong>tienen incluido un 5% de desperdicio</strong>. <button id="cerrar" class="btn-cerrar">Cerrar</button>';
+
+
+//Bolsa de 50 kg.
+let cemento = 50;
+
+//Coeficiente de desperdicio (5%)
+let desperdicio = 1.05;
+
+//Espesor del mortero en mts.
+let junta = 0.015;
+
+const ladrillos = ['Ladrillos Comunes 5x13x25', 'Ladrillos Huecos 6 agujeros 12x18x25', 'Ladrillos Huecos 9 agujeros 18x18x25']
+
+//Medidas de referencia ladrillo común en mts.
+let altoLadrillo = 0.05;
+let anchoLadrillo = 0.13;
+let largoLadrillo = 0.25;
+
 
 const productos = [
     {
         id: 1,
         nombre: 'Ladrillo Común 5x13x25',
-        precio: '112',
+        precio: 112,
         imagen: './images/1-ladrillo-comun-5x13x25.jpg'
     },
     {
         id: 2,
         nombre: 'Ladrillo Hueco 6A 8x18x33',
-        precio: '356',
+        precio: 356,
         imagen: './images/2-ladrillo-hueco-6a-8x18x33.jpg'
     },
     {
         id: 3,
         nombre: 'Ladrillo Hueco 6A 12x18x33',
-        precio: '446',
-        imagen: './images/3-ladrillo-hueco-6a-12x18x33.jpg'
+        precio: 446,
+        imagen: './images/3-ladrillo-hueco-6a-12x18x33.jpg',
+        checked: 'checked'
     },
     {
         id: 4,
         nombre: 'Ladrillo Hueco 9A 12x18x33',
-        precio: '451',
+        precio: 451,
         imagen: './images/4-ladrillo-hueco-9a-12x18x33.jpg'
     },
     {
         id: 5,
         nombre: 'Ladrillo Hueco 9A 18x18x33',
-        precio: '685',
+        precio: 685,
         imagen: './images/5-ladrillo-hueco-9a-18x18x33.jpg'
     },
     {
         id: 6,
         nombre: 'Ladrillo Portante 18x19x33',
-        precio: '819',
+        precio: 819,
         imagen: './images/6-ladrillo-portante-18x19x33.jpg'
     },
     {
         id: 7,
         nombre: 'Ladrillo Retak 10x25x50',
-        precio: '2882',
+        precio: 2882,
         imagen: './images/7-ladrillo-retak-10x25x50.jpg'
-    },
+    }
 ]
 
 const marcaCemento = [
@@ -174,7 +143,7 @@ function renderProducto (productsArray) {
                           <img src="${producto.imagen}" alt="${producto.nombre}">
                           <div class="sku">SKU <span id="${producto.id}">${producto.id}</span></div>
                           <div class="precio">$ <span>${producto.precio}</span></div>
-                          <input type="radio" class="productoSeleccion" name="ladrillos" id="${producto.id}" id="${producto.id}">`;
+                          <input type="radio" class="productoSeleccion" name="ladrillos" id="${producto.id}" id="${producto.id}" ${producto.checked}>`;
         productsContainer.appendChild(card);
     })
 }
@@ -189,8 +158,83 @@ function renderMarcaCemento (marcasArray) {
         opcion.innerHTML = `<option value="${marcaCemento.value}">${marcaCemento.nombre}</option>`;
         marcasContainer.appendChild(opcion);
     })
+    getInputId();
 }
 renderMarcaCemento(marcaCemento);
+
+
+function getInputId () {
+    let inputId = document.querySelectorAll('.productoSeleccion');
+    inputId.forEach (clickRadio => {
+        clickRadio.onClick = (e) => {
+            const productId = e.currentTarget.id;
+            const selectedProduct = productos.find(producto => producto.id == productId);
+            cartProducts.push(selectedProduct);
+            console.log(cartProducts);
+        }
+    })
+}
+
+
+
+//  *****************************************************************************
+
+//Funcion principal
+function calcularLadrillos() { 
+    const longitud = document.getElementById('inputLongitud').value;
+    const altura = document.getElementById('inputAltura').value;
+    const bolsaCemento = document.getElementById('marcas-cemento').value;
+
+    let resultadoPared = longitud * altura;
+
+    let ladrilloMortero = ((largoLadrillo + junta) * (altoLadrillo + junta));
+
+    let ladrillosCantidad = resultadoPared / ladrilloMortero * desperdicio;
+
+    // let cementoBolsas = Math.round((ladrillosCantidad / 3) / cemento);
+    let juntaSuperior = junta * largoLadrillo * anchoLadrillo;
+    let juntaLateral = junta * altoLadrillo * anchoLadrillo;
+    let juntaTotal = (juntaSuperior + juntaLateral) * ladrillosCantidad;
+
+    //Relacion 1:3 - 454 Kg./m3 de cemento
+    let dosificacionMortero = 454;
+    let cementoBolsas = dosificacionMortero * juntaTotal * desperdicio / cemento;
+
+    //Redondeo de valores
+    resultadoPared = Math.round(resultadoPared);
+    ladrillosCantidad = Math.round(ladrillosCantidad);
+    cementoBolsas = Math.ceil(cementoBolsas);
+    
+    let resultadoCalculo = `<div class="resultado-final">
+                            <h4>- RESULTADOS -</h4>
+                            <p>Superficie de pared: <strong>${resultadoPared} m<sup>2</sup></strong></p>
+                            <p>Cantidad: <strong>${ladrillosCantidad} unid.</strong></p>
+                            <p>Marca de cemento: <strong>${bolsaCemento}</strong></p>
+                            <p>Cantidad de bolsas: <strong>${cementoBolsas} unid. de 50 kg.</strong></p>
+                            <p></p></div>
+                            `;
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = resultadoCalculo;
+}
+
+let botonCalcular = document.getElementById('botonCalcular');
+botonCalcular.addEventListener('click', function() {
+    calcularLadrillos();
+});
+
+
+function borrarCalculo() {
+    document.querySelector('.resultado-final').remove();
+}
+
+let botonBorrar = document.getElementById('botonBorrar');
+botonBorrar.addEventListener('click', function() {
+    borrarCalculo();
+});
+
+// ******************************************************************************
+
 
 let footer = document.createElement('footer');
 footer.innerHTML = '<p>©Todos los derechos reservados.</p>';
