24f47233d18bed17a426dc3cf6b6a2c22253c9f8
pixelpando <pando100@gmail.com>
2024-05-21 13:10:47 -0300

    v0.65

--- js/main.js
+++ js/main.js
@@ -1,78 +1,11 @@
 // Header
 let header = document.getElementsByTagName('header');
-header[0].innerText = 'Calculadora de Materiales v0.59. Última Actualización: 03/05/2024';
+header[0].innerText = 'Calculadora de Materiales v0.65. Última Actualización: 21/05/2024';
 
 // Mensaje de advertencia
 let msgDesperdicio = document.getElementById('msg');
 msgDesperdicio.innerHTML = '<i class="fa-solid fa-triangle-exclamation fa-lg"></i>Los datos son a modo de referencia y pueden no ser exactos. Los cálculos <strong>tienen incluido un 5% de desperdicio</strong>. <button id="cerrar" class="btn-cerrar">Cerrar</button>';
 
-//Bolsa de 50 kg.
-let cemento = 50;
-
-//Coeficiente de desperdicio (5%)
-let desperdicio = 1.05;
-
-//Espesor del mortero en mts.
-let junta = 0.015;
-
-//Medidas de referencia ladrillo común en mts.
-let altoLadrillo = 0.12;
-let anchoLadrillo = 0.18;
-let largoLadrillo = 0.33;
-let nombreLadrillo = 'Ladrillo Hueco 6A 12x18x33';
-
-
-let productsContainer = document.getElementById('products-container');
-let marcasContainer = document.getElementById('marcas-cemento');
-let txtErrorProductos = 'ERROR. No se pudo cargar los productos. Intente nuevamente más tarde.';
-let msgErrorProductos = document.getElementById('msgErrorProductos');
-
-
-fetch('../db/ladrillos.json')
-.then(response => response.json())
-.then(data => {
-    data.forEach(producto => {
-        const card = document.createElement('div');
-        card.classList.add('producto');
-        card.innerHTML = `<label for="${producto.id}">${producto.nombre}</label>
-                          <img src="${producto.imagen}" alt="${producto.nombre}">
-                          <div class="sku">SKU <span id="${producto.id}">${producto.id}</span></div>
-                          <div class="precio">$ <span>${producto.precio}</span></div>
-                          <input type="radio" class="productoSeleccion" name="ladrillos" id="${producto.id}" id="${producto.id}" ${producto.checked}>`
-        productsContainer.appendChild(card);
-    })
-})
-
-
-const obtenerMarcasCementos = async () => {
-    let URL = '../db/cementos.json';
-    const marcaError = `<option disabled>ERROR. No se pudo cargar las marcas, intente más tarde.</option>`;
-    let renderizado = ``;
-
-    try {
-        let solicitud = await fetch(URL);
-        let response = await solicitud.json();
-
-        response.forEach(marcaCemento => {
-            let marcasContainer = document.getElementById('marcas-cemento');
-            renderizado += `<option value="${marcaCemento.value}">${marcaCemento.nombre}</option>`;
-            marcasContainer = renderizado;
-        })
-
-    } catch(err) {
-        console.log('Error detectado', err);
-        renderizado = marcaError;
-
-    } finally {
-        let marcasContainer = document.getElementById('marcas-cemento');
-        marcasContainer.innerHTML = renderizado;
-        // document.body.innerHTML = renderizado;
-    }
-}
-obtenerMarcasCementos();
-
-
-//Boton cerrar mensaje
 const botonCerrar = document.getElementById('cerrar');
 const mensaje = document.getElementById('msg');
 
@@ -89,104 +22,106 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-function getInputId() {
-    let inputId = document.querySelectorAll('.productoSeleccion');
-    inputId.forEach (clickRadio => {
-        clickRadio.onClick = (e) => {
-            const productId = e.currentTarget.id;
-            const selectedProduct = productos.find(producto => producto.id == productId);
-            cartProducts.push(selectedProduct);
-        }
+//Bolsa de 50 kg.
+let cemento = 50;
+
+//Coeficiente de desperdicio (5%)
+let desperdicio = 1.05;
+
+//Espesor del mortero en mts.
+let junta = 0.015;
+
+// Declaracion de variables globales y medidas de referencia en mts.
+let altoLadrillo = ''; //0.12
+let anchoLadrillo = ''; //0.18
+let largoLadrillo = ''; //0.33
+let nombreLadrillo = ''; // Ladrillo Hueco 6A 12x18x33 (Default)
+let precioLadrillo = ''; // 446
+
+//Sectores de contenido en el HTML
+let productsContainer = document.getElementById('products-container');
+let marcasContainer = document.getElementById('marcas-cemento');
+
+
+// Fetch para llamar a los Ladrillos
+fetch('../db/ladrillos.json')
+.then(response => response.json())
+.then(data => {
+    data.forEach(producto => {
+        altoLadrillo = producto.alto;
+        anchoLadrillo = producto.ancho;
+        largoLadrillo = producto.largo;
+        nombreLadrillo = producto.nombre;
+        precioLadrillo = producto.precio;
+        // console.log(altoLadrillo, anchoLadrillo, largoLadrillo, nombreLadrillo, precioLadrillo);
+        const card = document.createElement('div');
+        card.classList.add('producto');
+        card.innerHTML = `<label for="${producto.id}">
+                          <span>${producto.nombre}</span>
+                          <img src="${producto.imagen}" alt="${producto.nombre}">
+                          <div class="sku">SKU <span>${producto.id}</span></div>
+                          <div class="precio">$ <span>${producto.precio}</span></div>
+                          <input type="radio" class="productoSeleccion" name="ladrillos" id="${producto.id}" ${producto.checked  ? 'checked' : ''} data-alto="${producto.alto}" data-ancho="${producto.ancho}" data-largo="${producto.largo}" data-nombre="${producto.nombre}" data-precio="${producto.precio}">
+                          </label>`
+        productsContainer.appendChild(card);
+
     })
-}
+    actualizarData(altoLadrillo, anchoLadrillo, largoLadrillo, nombreLadrillo, precioLadrillo);
+})
 
+// Actualiza el atributo Data y las variables globales que seran tomados por la funcion principal
+function actualizarData() {
+    const inputs = document.querySelectorAll('.producto input');
+    inputs.forEach(input => {
+        input.addEventListener('click', () => {
+            altoLadrillo = parseFloat(input.dataset.alto);
+            anchoLadrillo = parseFloat(input.dataset.ancho);
+            largoLadrillo = parseFloat(input.dataset.largo);
+            nombreLadrillo = input.dataset.nombre;
+            precioLadrillo = parseFloat(input.dataset.precio);
 
-//Funcion principal
-function calcularLadrillos() { 
-    let longitud = document.getElementById('inputLongitud').value;
-    let altura = document.getElementById('inputAltura').value;
-    let bolsaCemento = document.getElementById('marcas-cemento').value;
-
-    let resultadoPared = longitud * altura;
-
-    let ladrilloMortero = ((largoLadrillo + junta) * (altoLadrillo + junta));
-
-    let ladrillosCantidad = resultadoPared / ladrilloMortero * desperdicio;
-
-    //let cementoBolsas = Math.round((ladrillosCantidad / 3) / cemento);
-    let juntaSuperior = junta * largoLadrillo * anchoLadrillo;
-    let juntaLateral = junta * altoLadrillo * anchoLadrillo;
-    let juntaTotal = (juntaSuperior + juntaLateral) * ladrillosCantidad;
-
-    //Relacion 1:3 - 454 Kg./m3 de cemento
-    let dosificacionMortero = 454;
-    let cementoBolsas = dosificacionMortero * juntaTotal * desperdicio / cemento;
-
-    //Redondeo de valores
-    resultadoPared = Math.round(resultadoPared);
-    ladrillosCantidad = Math.round(ladrillosCantidad);
-    cementoBolsas = Math.ceil(cementoBolsas);
-    
-    let resultadoCalculo = `<div class="resultado-final">
-                            <h4>- RESULTADOS -</h4>
-                            <p>Producto: <strong>${nombreLadrillo}</strong></p>
-                            <p>Superficie de pared: <strong>${resultadoPared} m<sup>2</sup></strong></p>
-                            <p>Cantidad: <strong>${ladrillosCantidad} unid.</strong></p>
-                            <p>Marca de cemento: <strong>${bolsaCemento}</strong></p>
-                            <p>Cantidad de bolsas: <strong>${cementoBolsas} unid. de 50 kg.</strong></p>
-                            <button class="agregar-carrito" id="agregarAlCarrito">Agregar al Carrito</button>
-                            </div>`;
-
-    const resultado = document.getElementById('resultado');
-    resultado.innerHTML = resultadoCalculo;
-
-    tryAnalisis(longitud, altura);
-}
-
-
-//Botones
-let botonCalcular = document.getElementById('botonCalcular');
-botonCalcular.addEventListener('click', () => {
-    calcularLadrillos();
-});
-
-function borrarCalculo() {
-    let resultadoFinal = document.querySelector('.resultado-final');
-    resultadoFinal.remove();
-    let mensajeAnalisis = document.querySelector('#analisis-resultado');
-    mensajeAnalisis.innerText = '';
-    mensajeAnalisis.classList.remove('mensaje','error','correcto','ocultar-anim');
-}
-
-let botonBorrar = document.getElementById('botonBorrar');
-botonBorrar.addEventListener('click', function() {
-    borrarCalculo();
-});
-
-
-//Agrega un event listener a cada radio button
-const radioSelection = document.querySelectorAll('input[type="radio"].productoSeleccion');
-
-radioSelection.forEach(radio => {
-    radio.addEventListener('click', function() {
-        const idSeleccionado = parseInt(this.id);
-        const productoSeleccionado = productos.find(producto => producto.id === idSeleccionado);
-        
-        if (productoSeleccionado) {
-            altoLadrillo = productoSeleccionado.alto;
-            anchoLadrillo = productoSeleccionado.ancho;
-            largoLadrillo = productoSeleccionado.largo;
-            nombreLadrillo = productoSeleccionado.nombre;
-        }
+            console.log(altoLadrillo);
+            console.log(anchoLadrillo);
+            console.log(largoLadrillo);
+            console.log(nombreLadrillo);
+            console.log(precioLadrillo);
+        });
     });
-});
+}
 
-//Mensaje de estado de calculo con try-catch-finally
+// Async con Try-Catch-Finally para llamar a las marcas de cemento
+const obtenerMarcasCementos = async () => {
+    let URL = '../db/cementos.json';
+    const marcaError = `<option disabled>ERROR. No se pudo cargar las marcas, intente más tarde.</option>`;
+    let renderizado = ``;
+
+    try {
+        let solicitud = await fetch(URL);
+        let response = await solicitud.json();
+
+        response.forEach(marcaCemento => {
+            // let marcasContainer = document.getElementById('marcas-cemento');
+            renderizado += `<option value="${marcaCemento.value}">${marcaCemento.nombre}</option>`;
+            // marcasContainer = renderizado;
+        })
+
+    } catch(err) {
+        console.log('Error detectado', err);
+        renderizado = marcaError;
+
+    } finally {
+        let marcasContainer = document.getElementById('marcas-cemento');
+        marcasContainer.innerHTML = renderizado;
+    }
+}
+obtenerMarcasCementos();
+
+
+// Funcion Try-Catch-Finally
 function tryAnalisis(longitud, altura) {
     let analisis = '';
     let analisisResultado = document.getElementById('analisis-resultado');
     try {
-        // if ((longitud && altura) > 0) {
         if (longitud > 0 || altura > 0) {
             analisisResultado.classList.add('mensaje','correcto');
             analisis = 'El cálculo se realizó correctamente'; 
@@ -209,7 +144,71 @@ function tryAnalisis(longitud, altura) {
 }
 
 
-//Footer
+//Funcion principal
+function calcularLadrillos() { 
+    console.log(altoLadrillo, anchoLadrillo, largoLadrillo, nombreLadrillo, precioLadrillo);
+    let longitud = document.getElementById('inputLongitud').value;
+    let altura = document.getElementById('inputAltura').value;
+    let bolsaCemento = document.getElementById('marcas-cemento').value;
+
+    let resultadoPared = longitud * altura;
+
+    let ladrilloMortero = ((largoLadrillo + junta) * (altoLadrillo + junta));
+
+    let ladrillosCantidad = resultadoPared / ladrilloMortero * desperdicio;
+
+    // let cementoBolsas = Math.round((ladrillosCantidad / 3) / cemento);
+    let juntaSuperior = junta * largoLadrillo * anchoLadrillo;
+    let juntaLateral = junta * altoLadrillo * anchoLadrillo;
+    let juntaTotal = (juntaSuperior + juntaLateral) * ladrillosCantidad;
+
+    //Relacion 1:3 - 454 Kg./m3 de cemento
+    let dosificacionMortero = 454;
+    let cementoBolsas = dosificacionMortero * juntaTotal * desperdicio / cemento;
+
+    //Redondeo de valores
+    resultadoPared = Math.round(resultadoPared);
+    ladrillosCantidad = Math.round(ladrillosCantidad);
+    cementoBolsas = Math.ceil(cementoBolsas);
+    
+    let resultadoCalculo = `<div class="resultado-final">
+                            <h4>- RESULTADOS -</h4>
+                            <p>Producto: <strong>${nombreLadrillo}</strong></p>
+                            <p>Superficie de pared: <strong>${resultadoPared} m<sup>2</sup></strong></p>
+                            <p>Cantidad: <strong>${ladrillosCantidad} unid.</strong></p>
+                            <p>Precio: <strong>$ ${precioLadrillo}</strong></p>
+                            <p>Marca de cemento: <strong>${bolsaCemento}</strong></p>
+                            <p>Cantidad de bolsas: <strong>${cementoBolsas} unid. de 50 kg.</strong></p>
+                            <button class="agregar-carrito" id="agregarAlCarrito">Agregar al Carrito</button>
+                            </div>`;
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = resultadoCalculo;
+
+    tryAnalisis(longitud, altura);
+}
+
+// Boton Calcular
+let botonCalcular = document.getElementById('botonCalcular');
+botonCalcular.addEventListener('click', () => {
+    calcularLadrillos();
+});
+
+// Boton Reset
+let botonReset = document.getElementById('botonReset');
+botonReset.addEventListener('click', () => {
+    let resultadoFinal = document.querySelector('.resultado-final');
+    if (resultadoFinal === null) {
+        } else {
+        resultadoFinal.remove();
+    }
+    let mensajeAnalisis = document.querySelector('#analisis-resultado');
+    mensajeAnalisis.innerText = '';
+    mensajeAnalisis.classList.remove('mensaje','error','correcto','ocultar-anim');
+});
+
+
+// Footer
 let footer = document.createElement('footer');
 footer.innerHTML = '<p>©Todos los derechos reservados.</p>';
 document.body.appendChild(footer);
